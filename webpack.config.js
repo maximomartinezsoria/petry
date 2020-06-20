3a99464d0208d7fe8c892d02f899a43ce2b7fde1
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2020-06-20 14:10:34 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+exports.default = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ]
+}
