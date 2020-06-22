a9ff12201685aa2bff1660f2877b699fcf809feb
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2020-06-22 18:45:10 -0300

    Fetch categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,21 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import { List, Item } from './styles'
 import { Category } from '../Category'
+import { url } from '../../helpers'
 
-export const ListOfCategories = () => {
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    fetch('')
+    setLoading(true)
+    fetch(`${url}/categories/`)
       .then(res => res.json())
       .then(data => setCategories(data))
-  })
+    setLoading(false)
+  }, [])
 
-  return (
-    <List>
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      if (showFixed !== newShowFixed) setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
-        categories.map(category => <Item key={category}><Category /></Item>)
+        loading
+          ? <Item><Category/></Item>
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  if (loading) return 'Loading...'
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
